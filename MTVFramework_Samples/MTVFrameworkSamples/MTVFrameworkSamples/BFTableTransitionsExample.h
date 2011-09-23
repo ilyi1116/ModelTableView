66597290e3258ed9d183d59f9aa9c7009d9e41ca
Vladislav Alekseev <beefon@yandex-team.ru>
2011-09-23 10:38:25 +0400

    Added support for UITableView extended animations, added example for it.

---
+++ MTVFramework_Samples/MTVFrameworkSamples/MTVFrameworkSamples/BFTableTransitionsExample.h
@@ -0,0 +1,13 @@
+//
+//  BFTableTransitionsExample.h
+//  MTVFrameworkSamples
+//
+//  Created by Алексеев Владислав on 23.09.11.
+//  Copyright (c) 2011 Yandex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BFTableTransitionsExample : UIViewController
+
+@end
